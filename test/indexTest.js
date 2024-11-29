07780e55961f1f929ba5789c353f865a42c75d5c
adnan <106653813+ibn-filistiin@users.noreply.github.com>
2024-11-29 14:53:45 +0300

    js func lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -7,6 +7,8 @@ describe('shout(string)', function() {
   })
 })
 
+
+
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello');
